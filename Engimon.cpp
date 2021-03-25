9ece7df1bd5c01aa8b6a152576e6be926d6c4a8e
arsa-dg <arsaadg891@gmail.com>
2021-03-25 21:40:31 +0700

    level on costructor engimon

--- Engimon.cpp
+++ Engimon.cpp
@@ -6,14 +6,14 @@ using namespace std;
 int Engimon::maxLevel = 50;
 int Engimon::maxCumulativeExperience = 5000;
 
-Engimon::Engimon(string _name, string _species, string _parentName, string _parentSpecies){
+Engimon::Engimon(string _name, string _species, string _parentName, string _parentSpecies, int _level){
     this->name = _name;
     this->species = _species;
     this->parentName = _parentName;
     this->parentSpecies = _parentSpecies;
-    this->level = 1;
+    this->level = _level;
     this->experience = 0;
-    this->cumulativeExperience = 0;
+    this->cumulativeExperience = 100*_level;
 }
 
 Engimon::Engimon(const Engimon& e){
@@ -73,6 +73,18 @@ int Engimon::getCountElement(){
 
 int Engimon::getCountSkill(){
     return this->countSkill;
+}
+
+Element* Engimon::getElements(){
+    return this->elements;
+}
+
+Skill* Engimon::getLeanedSkills(){
+    return this->learnedSkills;
+}
+
+void Engimon::interact(){
+    cout << "Hello bos" << endl;
 }
 
 void Engimon::addExperience(int _exp){
--- Species.hpp
+++ Species.hpp
@@ -6,37 +6,67 @@
 #include "Skill.hpp"
 #include "Element.hpp"
 
+// element fire
 class Firemon : public Engimon {
     public:
-        Firemon(string, string, string);
+        Firemon(string, string, string, int);
         ~Firemon();
         void interact();
 };
 
+// element water
 class Watermon : public Engimon {
     public:
-        Watermon(string, string, string);
+        Watermon(string, string, string, int);
         ~Watermon();
         void interact();
 };
 
+// element electric
 class Electricmon : public Engimon {
     public:
-        Electricmon(string, string, string);
+        Electricmon(string, string, string, int);
         ~Electricmon();
         void interact();
 };
+
+// element ground
 class Groundmon : public Engimon {
     public:
-        Groundmon(string, string, string);
+        Groundmon(string, string, string, int);
         ~Groundmon();
         void interact();
 };
 
+// element ice
 class Icemon : public Engimon {
     public:
-        Icemon(string, string, string);
+        Icemon(string, string, string, int);
         ~Icemon();
+        void interact();
+};
+
+// element fire, electric
+class Itachimon : public Engimon {
+    public:
+        Itachimon(string, string, string, int);
+        ~Itachimon();
+        void interact();
+};
+
+// element water, ice
+class Telermon : public Engimon {
+    public:
+        Telermon(string, string, string, int);
+        ~Telermon();
+        void interact();
+};
+
+// element water, ground
+class Narutomon : public Engimon {
+    public:
+        Narutomon(string, string, string, int);
+        ~Narutomon();
         void interact();
 };
 
